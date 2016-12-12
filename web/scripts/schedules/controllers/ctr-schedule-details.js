3dfde9da2f47632436d13bd6beacbe340e5ccec4
Ezequiel Conte <ezequielc@gmail.com>
2016-12-12 11:23:12 -0500

    work in progress

--- web/scripts/schedules/controllers/ctr-schedule-details.js
+++ web/scripts/schedules/controllers/ctr-schedule-details.js
@@ -97,29 +97,23 @@ angular.module('risevision.schedules.controllers')
         $scope.populateEvents();
       },true);
 
-     //   var date = new Date();
-    // var d = date.getDate();
-    // var m = date.getMonth();
-    // var y = date.getFullYear();
-    
-    //   $scope.calendarEvents = [
-    //    [
-    //   {title: 'All Day Event',start: new Date(y, m, 1)},
-    //   {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-    //   {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-    //   {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-    //   {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-    //   {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-    // ]
-    //   ];
       var RECURRENCE = {
         DAILY: "Daily",
         WEEKLY: "Weekly",
         MONTHLY: "Monthly",
         YEARLY: "Yearly"
       };
+      var DAY_OF_WEEK = {
+        SUNDAY: 'Sun',
+        MONDAY: 'Mon',
+        TUESDAY: 'Tue',
+        WEDNESDAY: 'Wed',
+        THURSDAY: 'Thu',
+        FRIDAY: 'Fri',
+        SATURDAY: 'Sat',
+      };
 
-      function getDates(startDate, stopDate) {
+      var getDates = function (startDate, stopDate) {
         var dateArray = [];
         var currentDate = moment(startDate);
         while (currentDate <= stopDate) {
@@ -127,7 +121,37 @@ angular.module('risevision.schedules.controllers')
             currentDate = moment(currentDate).add(1, 'days');
         }
         return dateArray;
-    }
+      }
+
+      var _diffDays = (fromDate, toDate) => {
+        if (!fromDate || !toDate) {
+          return -1;
+        }
+        var msecInOneDay = 1000 * 60 * 60 * 24;
+        return parseInt((toDate.getTime() - fromDate.getTime()) / msecInOneDay);
+      }
+
+      var _diffMonths = (fromDate, toDate) => {
+        return parseInt(((toDate.getYear() - fromDate.getYear()) * 12) + toDate.getMonth() - fromDate.getMonth());
+      }
+
+      var _diffWeeks = (fromDate, toDate) => {
+        var days = _diffDays(fromDate, toDate);
+        var weeks = (days + fromDate.getDay()) / 7;
+
+        return parseInt(weeks);
+      }
+
+      var _isRecurrenceDay = function (weekday, recurrenceDaysOfWeek) {
+        var dayOfWeeklbl = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'][weekday];
+        var currDayCode = DAY_OF_WEEK[dayOfWeeklbl];
+
+        if(recurrenceDaysOfWeek && recurrenceDaysOfWeek.indexOf(currDayCode) >= 0) {
+          return true;
+        } else {
+          return false;
+        }
+      }
 
       var calendarStart, calendarEnd;
       $scope.populateEvents = function() {
@@ -135,54 +159,72 @@ angular.module('risevision.schedules.controllers')
         var events = [];
         angular.forEach($scope.schedule.content,function(item){
 
-          if (item.timeDefined) { 
-            if (item.recurrenceType == RECURRENCE.DAILY) {
-              var calendarRange = getDates(calendarStart,calendarEnd);
-              angular.forEach(calendarRange,function(date){
-                if ((!item.startDate || moment(date) >= moment(item.startDate)) && (!item.endDate || moment(date) <= moment(item.endDate))) {
-                  var event = {title: item.name, item:item};
-                  event.start = item.startTime?moment(date).add(moment.duration(moment(item.startTime).format('HH:mm'))):moment(date);
-                  event.end = item.endTime?moment(date).add(moment.duration(moment(item.endTime).format('HH:mm'))):moment(date).add(1,'days');
-                  
-                  if (item.startTime) {
-                    var days = moment(item.startDate).diff(moment(date),'days')
-                    if (parseInt(days % item.recurrenceFrequency) == 0) {
-                      events.push(event);  
-                    }
-                  } else {
-                    events.push(event)  
-                  }                  
-                }
-              });
-            } else if (item.recurrenceType == RECURRENCE.WEEKLY) {
-
-            } else if (item.recurrenceType == RECURRENCE.MONTHLY) {
-
-            } else if (item.recurrenceType == RECURRENCE.YEARLY) {
-
-            }
-
-          } else { // 24/7
+          if (item.recurrenceType == RECURRENCE.DAILY) {
             var calendarRange = getDates(calendarStart,calendarEnd);
             angular.forEach(calendarRange,function(date){
-              var event = {
-                title: item.name, 
-                start: date,
-                end: moment(date).add(24, 'hours'),
-                item:item
+              if ((!item.startDate || moment(date) >= moment(item.startDate)) && (!item.endDate || moment(date) <= moment(item.endDate))) {
+                var event = {title: item.name, item:item};
+
+                if (item.timeDefined) {
+                  event.start = item.startTime?moment(date).add(moment.duration(moment(item.startTime).format('HH:mm'))):moment(date);
+                  event.end = item.endTime?moment(date).add(moment.duration(moment(item.endTime).format('HH:mm'))):moment(date).add(1,'days');                
+                } else {
+                  event.start = date;
+                  event.end = moment(date).add(24, 'hours');
+                }
+                
+                if (item.startDate) {
+                  var days = moment(item.startDate).diff(moment(date),'days')
+                  if (parseInt(days % item.recurrenceFrequency) == 0) {
+                    events.push(event);  
+                  }
+                } else {
+                  events.push(event)  
+                }                  
               }
-              events.push(event)
             });
+          } else if (item.recurrenceType == RECURRENCE.WEEKLY) {
+            var calendarRange = getDates(calendarStart,calendarEnd);
+            angular.forEach(calendarRange,function(date){
+              if ((!item.startDate || moment(date) >= moment(item.startDate)) && (!item.endDate || moment(date) <= moment(item.endDate))) {
+                var event = {title: item.name, item:item};
+
+                if (item.timeDefined) {
+                  event.start = item.startTime?moment(date).add(moment.duration(moment(item.startTime).format('HH:mm'))):moment(date);
+                  event.end = item.endTime?moment(date).add(moment.duration(moment(item.endTime).format('HH:mm'))):moment(date).add(1,'days');                
+                } else {
+                  event.start = date;
+                  event.end = moment(date).add(24, 'hours');
+                }
+                
+                if (item.startDate) {
+                  var weeks = _diffWeeks(moment(item.startDate).toDate(), moment(date).toDate());
+                  if (parseInt(weeks % item.recurrenceFrequency) != 0) {
+                    return;
+                  }
+                  var weekday = moment(date).toDate().getDay();
+                  if (!_isRecurrenceDay(weekday, item.recurrenceDaysOfWeek)) {
+                    return;
+                  }
+                  events.push(event);  
+                }                
+              }
+            });
+          } else if (item.recurrenceType == RECURRENCE.MONTHLY) {
+
+          } else if (item.recurrenceType == RECURRENCE.YEARLY) {
+
           }
         });
         $scope.calendarEvents[0] = events;
+        console.log($scope.schedule.content,$scope.calendarEvents);
       }
 
 
       $scope.calendarOptions = {
         allDaySlot: false,
         defaultView: 'agendaWeek',
-        height: 650,
+        height: 550,
         editable: false,
         eventColor: '#4ab767',
         scrollTime: '00:00',
@@ -206,7 +248,6 @@ angular.module('risevision.schedules.controllers')
           
         },
         viewRender: function(view, element) {
-            console.log("View Changed: ", view.start, view.end);
             calendarStart = view.start;
             calendarEnd = view.end;
             $scope.populateEvents();
