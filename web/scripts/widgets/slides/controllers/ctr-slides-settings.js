f8f8579e3f9f74d3a40604efc333e0d272617926
Rudi Bravo <rudibravo@gmail.com>
2018-03-27 17:59:11 -0300

    Fixed componentId not working [stage-4]

--- web/scripts/widgets/slides/controllers/ctr-slides-settings.js
+++ web/scripts/widgets/slides/controllers/ctr-slides-settings.js
@@ -3,7 +3,7 @@ angular.module('risevision.widgets.slides')
   .controller('SlidesSettingsController', ['$scope', '$rootScope',
     function ($scope, $rootScope) {
       
-      $scope.$watch( 'settings.additionalParams.screenName', function(screenName) {
+      $scope.$watch( 'settings.additionalParams.url', function(screenName) {
         if (screenName) {
           $scope.settings.additionalParams.componentId = 'rise-slides-' + Math.random().toString().substring(2);
         }
